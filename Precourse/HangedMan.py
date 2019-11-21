f8de7b57707b8bec7f87c5753c060ed906a8bed9
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2019-11-21 15:08:17 +0200

    completed more excerises

--- Precourse/HangedMan.py
+++ Precourse/HangedMan.py
@@ -1,44 +1,77 @@
-
-
-import HangedGameUtils
 import random
+import re
 
-
-def change_char(s, p, r):
-    return s[:p]+r+s[p+1:]
-
-
-print (HangedGameUtils.test)
-word = HangedGameUtils.words[random.randint(0,3)]
-size = len(word)
+while_loop = 0
 tries = 10
-userWord = "*"*size
 
-
-
-while (tries>0) and (userWord != word):
-    print("You have " , tries , " guesses")
-    print("the word is ")
-    print (userWord)
-    guess = input("What is your guess?\n").lower()
-    if((guess>='a' and guess<= 'z') or (guess>='A' and guess<='Z')) and (len(guess)== 1):
-        found = False
-        for idx,letter in enumerate(word):
-            if letter == guess:
-                userWord = change_char(userWord, idx ,guess)
-                found = True
-
-        if not found:
-            tries -= 1
+# //charlist randomword 1
+def print_word(char, word, is_finish):
+    func_word = list(word)
+    count = 0
+    if not char:
+        for t in range(len(word)):
+            print("*", end=" ")
     else:
-        print("invalid input")
+        for j in range(len(func_word)):
+            y = 0
+            for h in range(len(char)):
+                if char[h] == func_word[j]:
+                    print(func_word[j], end=" ")
+                    y = 0
+                    count += 1
+                else:
+                    y = y + 1
+            if y == len(char) and char[y - 1] != func_word[j]:
+                print("*", end=" ")
+        if is_finish == 1:  # return count is a flag for me if i want to use this function just to print or also make desicions
+            return count
 
 
-if tries==0:
-     print("game over, you lose")
-else:
-    print("game over, you won")
+print("""////////////////     Welcome to the HANGMEN game       \\\\\\\\\\\\\\\\\\
+ ___________.._______
+| .__________))______|
+| | / /      ||
+| |/ /       ||
+| | /        ||.-''.
+| |/         |/  _  \
+| |          ||  `/,|
+| |          (\\`_.'
+| |         .-`--'.
+| |        /Y . . Y\
+| |       // |   | \\
+| |      //  | . |  \\
+| |     ')   |   |   (`
+| |          ||'||
+| |          || ||
+| |          || ||
+| |          || ||
+| |         / | | \
+""""""""""|_`-' `-' |""")
 
-
-    
-
+word_list = ["bull", "horse", "metadore", "spaceshipe", "alphabet"]
+random_word = random.choice(word_list)
+print("\n")
+for i in range(len(random_word)):
+    print("*", end=" ")
+char_list = []
+while while_loop == 0:
+    user_char = input("\n\nEnter a char to discover: ")
+    if len(user_char) == 1 and re.search("[@_!#$%^&*()<>?/|}{~:1234567890]", user_char) is None:
+        user_char = user_char.lower()
+        tries = tries - 1
+        if user_char in random_word:
+            char_list.append(user_char)
+            print("\nWOW you are good!!!\n")
+            x = print_word(char_list, random_word, 1)
+            if x == len(random_word):
+                exit("\nYou got it!!! brilliant guesses")
+            elif tries == 0:
+                exit("\nNo more tries left")
+        else:
+            print_word(char_list, random_word, 0)
+            if tries == 0:
+                exit("\nNo more tries left")
+    else:
+        print("Please enter only one character\n" + "You have " + str(tries) + " guesses\n" + "The word is:\n")
+        print_word(char_list, random_word, 0)
+        print("\nWhat is your guesses?\n")
--- Precourse/student.py
+++ Precourse/student.py
@@ -1,39 +1,60 @@
 import random
+import re
 
-
-print("~ welcome to WAR ~")
-username = input('please enter your name:')
-money = 50
-flag = True
-print('hello ', username,'you currently have ' + str(money) + ' dollars.')
-
-while flag:
-    cOntinue = int(input('1. if you want to bet.\n2. if you want to take the many and go.\n choose 1 \ 2:\n'))
-    if cOntinue ==1:
-        print("let's go!!")
-        print(username, ' you currently have ' + str(money) + ' dollars.')
-        userBet = int(input('place your bet for the next round:you can bet between 1 to ' + str(money) + ' $ --> \n'))
-
-        if money >= userBet:
-            computerCard = random.randint(1,12)
-            userCard = random.randint(1,12)
-            print('Computer Card is: '+str(computerCard))
-            print('User Card is: ' + str(userCard))
-
-            if userCard > computerCard:
-                money += userBet
-            else:
-                money -= userBet
-        if money == 0:
-            print('You dont have money, go home')
+choise = 1
+user_bank = 50
+print("/////////----------   Welcome To War  -------------\\\\\\\\\\")
+user_name = input("please enter your name: ")
+print("Hello " + str(user_name) + " You currently have " + str(user_bank) + " ILS")
+while choise == 1:
+    loop = 0
+    print("\nPlace your bet for the next round: 1 to " + str(user_bank))
+    bet = input()
+    if bet.isdigit() is False:
+        print("\nplease enter only numbers!!!\n" + "You currently have " + str(user_bank) + " ILS\n")
+    else:
+        bet = int(bet)
+        if bet > user_bank or bet < 0:
+            print("\nI said between 1 to " + str(user_bank) + " and you typed " + str(bet) + "!!\n"
+                  + "I don't play with liars!!! Bye")
             exit()
         else:
-            print('Your bet is to high')
+            user_card = random.randint(1, 12)
+            comp_card = random.randint(1, 12)
+            if user_card > comp_card:
+                user_bank = user_bank + bet
+                while loop == 0:
+                    choise = input("\nYour card is " + str(user_card) + " And my card is " + str(comp_card)
+                                   + "\n" + "You Won " + str(bet) + " And now you have " + str(user_bank)
+                                   + "\n" + "What would you like to do?" + "\n" + "1. Play another round"
+                                   + "\n" + "2. Leave with my money :-)")
+                    if choise.isdigit():
+                        choise = int(choise)
+                        if choise == 1 or choise == 2:
+                            loop = 1
+                        else:
+                            choise = 0
+                    else:
+                        loop = 0
+            else:
+                user_bank = user_bank - bet
+                if user_bank > 0:
+                    while loop == 0:
+                        choise = input("\nYour card is " + str(user_card) + " And my card is " + str(comp_card)
+                                       + "\n" + "You lose " + str(bet) +" And now you have " + str(user_bank)
+                                       + "\n" + "What would you like to do?" + "\n" + "1. Play another round"
+                                       + "\n" + "2. Leave with my money :-)")
+                        if choise.isdigit():
+                            choise = int(choise)
 
-        print('now you have ' + str(money) + ' $ \n')
-
-    elif cOntinue == 2:
-        print("you are a coward! Bye.")
-        flag = False
-    elif cOntinue != 1 and cOntinue != 2:
-        print('read again!')
+                            if choise == 1 or choise == 2:
+                                loop = 1
+                            else:
+                                choise = 0
+                        else:
+                            loop = 0
+                else:
+                    print("\nYour card is " + str(user_card) + " And my card is " + str(comp_card) + "\n"
+                          + "You lost " + str(bet) + " And now you have " + str(user_bank)
+                          + "\n" + "You are broke... Bye Bye")
+                    exit()
